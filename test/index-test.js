319adb1e3285189bac6e5b83957f6dde3502baad
tommytnez <tommytnez@yahoo.com>
2017-01-05 15:22:19 -0800

    readfile

--- test/index-test.js
+++ test/index-test.js
@@ -2,10 +2,11 @@
 
 // const fs = require('fs');
 const expect = require('chai').expect;
+var
 
-describe('grabBit', function(){
-  it('it should exist', function(){
-    let grabBit = require('../index.js');
+describe('should show house.bmp', function(){
+  it('should exist', function(){
+    let grabBit = require('./assets/house.bmp');
     expect(grabBit).to.not.be.undefined;
   });
 });
