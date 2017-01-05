55d23742a5495fbf7980901f624fdb5a8e31a42f
tommytnez <tommytnez@yahoo.com>
2017-01-05 14:15:16 -0800

    started TDD format

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 'use strict';
+
+const fs = require('fs');
+const EventEmitter = require('events').EventEmitter;
--- test/index-test.js
+++ test/index-test.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// const fs = require('fs');
+const expect = require('chai').expect;
+
+describe('grabBit', function(){
+  it('it should exist', function(){
+    let grabBit = require('../index.js');
+    expect(grabBit).to.not.be.undefined;
+  });
+});
