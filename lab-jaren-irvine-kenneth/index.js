527fc997adf4533eb68d69d3d4b2d676ad08e996
irvinemd55 <irvinedowning@gmail.com>
2017-01-06 15:04:46 -0800

    transformer added

--- lab-jaren-irvine-kenneth/index.js
+++ lab-jaren-irvine-kenneth/index.js
@@ -2,19 +2,13 @@
 
 const readBitmap = require('./lib/read-bitmap.js');
 const writeBitmap = require('./lib/write-bitmap.js');
+const transformer = require('./lib/transforms.js');
 
 const main = module.exports = function() {
   return readBitmap('../assets/bitmap.bmp', function(err, bitmap) {
     if (err) return console.error(err);
     // this is where we do our transform
-    bitmap.colorArray.fill(0);
-    for (var i = 0; i < bitmap.colorArray.length; i += 4){
-      bitmap.colorArray[i] = 0; // blue
-      bitmap.colorArray[i+1] = 0; // green
-      bitmap.colorArray[i+2] = 0; // red
-      bitmap.colorArray[i+3] = 0; // alpha
-    }
-
+    transformer.randomColors(bitmap);
     writeBitmap('outputs/bitmap-out.bmp', bitmap, function(err){
       if (err) return console.error(err);
     });
---
+++ lab-jaren-irvine-kenneth/lib/transforms.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const transformer = module.exports = {};
+
+transformer.randomColors = function(bitmap) {
+  for (var i = 0; i < bitmap.colorArray.length; i+= 4) {
+    bitmap.colorArray[i] = Math.floor(Math.random() * 255) + 1;
+    bitmap.colorArray[i + 1] = Math.floor(Math.random() * 255) + 1;
+    bitmap.colorArray[i + 2] = Math.floor(Math.random() * 255) + 1;
+  }
+};
