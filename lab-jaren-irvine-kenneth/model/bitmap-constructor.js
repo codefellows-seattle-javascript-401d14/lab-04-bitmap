8aecc2a6db54b5b6f186aa42f5e5931917b440f1
irvinemd55 <irvinedowning@gmail.com>
2017-01-05 17:41:24 -0800

    constructor started

--- lab-jaren-irvine-kenneth/model/bitmap-constructor.js
+++ lab-jaren-irvine-kenneth/model/bitmap-constructor.js
@@ -5,8 +5,10 @@ module.exports = function(data) {
   this.format = data.readUInt32LE(28);
   this.width = data.readUInt32LE(18);
   this.height = data.readUInt32LE(22);
-  this.red = data.readUInt32LE(54);
-  this.green = data.readUInt32LE(58);
-  this.blue = data.readUInt32LE(62);
-  this.alpha = data.readUInt32LE(66);
+  // this.red = data.readUInt32LE(54);
+  // this.green = data.readUInt32LE(58);
+  // this.blue = data.readUInt32LE(62);
+  // this.alpha = data.readUInt32LE(66);
+  this.offset = data.readUInt32LE(10);
+  this.colorArray = data.slice(54,this.offset);
 };
