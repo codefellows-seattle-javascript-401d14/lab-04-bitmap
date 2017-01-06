0e6d28d2762a77c778dc521c7302e977ec1b7d10
tommytnez <tommytnez@yahoo.com>
2017-01-06 14:31:51 -0800

    wrote file

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 'use strict';
 
+const writeFile = require('./lib/bitmap-writer.js');
 const readBitmap = require('./lib/bitmapreader.js');
 // const EventEmitter = require('events').EventEmitter;
 
 
 const main = module.exports = function(){
-  return readBitmap('./assets/house.bmp', function(err, data) {
+  return readBitmap('./assets/house.bmp', function(err, bitMap) {
     if (err) return console.error(err);
-
-    console.log(data);
+    console.log(bitMap);
+    /// transform
+    
+    return writeFile('./outputs/output.bmp', bitMap, function(err) {
+      if (err) return console.error(err);
+    });
   });
 };
 console.log(main());
--- model/bit-read-constructor.js
+++ model/bit-read-constructor.js
@@ -1,8 +1,15 @@
 'use strict';
 
 
-module.exports = function(data) {
-  this.buffs = data;
-  this.colors = data.readUIntLE(0,34);
-
+const BitMap = module.exports = function(data) {
+  this.filesize = data.readUInt32LE(2);
+  this.format = data.readUInt32LE();
+  this.width = data.readUInt32LE(22);
+  this.height = data.readUInt32LE();
+  this.offset = data.readUInt32LE(10);
+  this.colorArray = data.slice(54,this.offset);
+  this.buf = data;
+  console.log(data);
 };
+
+BitMap.prototype.random
