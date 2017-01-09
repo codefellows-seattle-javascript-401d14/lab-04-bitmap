8bfe60495a706223d6d428a87918d916035ae5f7
jjron <jaren.escueta@gmail.com>
2017-01-08 22:45:52 -0800

    CLI works with valid input
    Fails silently with invalid input, not sure why...

--- lab-jaren-irvine-kenneth/index.js
+++ lab-jaren-irvine-kenneth/index.js
@@ -4,22 +4,27 @@ const readBitmap = require('./lib/read-bitmap.js');
 const writeBitmap = require('./lib/write-bitmap.js');
 const transformer = require('./lib/transforms.js');
 
-const main = module.exports = function() {
+const main = module.exports = function(argv) {
+  var names = ['random', 'grey', 'invert'];
+  if (!argv || argv.length !== 5) return 'Usage error: node index.js <transformName> <inputFilePath> <outputFilePath>';
+  if (names.indexOf(argv[2]) === -1) return 'Usage error: transform name not recognized';
   //reads our constructor
-  return readBitmap('../assets/bitmap.bmp', function(err, bitmap) {
+  return readBitmap(argv[3], function(err, bitmap) {
     if (err) return console.error(err);
     // this is where we do our transform
-    // console.log(bitmap.id);
-    //transformer.randomColors(bitmap);
-    //transformer.greyScale(bitmap);
-    transformer.invertColors(bitmap);
-    //follow up with a write to the new ouput folder
-    writeBitmap('outputs/main-output.bmp', bitmap, function(err){
+    if (argv[2] === 'random')
+      transformer.randomColors(bitmap);
+    if (argv[2] === 'grey')
+      transformer.greyScale(bitmap);
+    if (argv[2] === 'invert')
+      transformer.invertColors(bitmap);
+    //follow up with a write to the new output folder
+    writeBitmap(argv[4], bitmap, function(err){
       if (err) return console.error(err);
     });
   });
 };
 
-main();
+main(process.argv);
 
 //promises of the future
