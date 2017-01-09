5551e06935faca8adb1fc585888b282da915db2f
jjron <jaren.escueta@gmail.com>
2017-01-08 22:31:16 -0800

    constructor comments

--- lab-jaren-irvine-kenneth/model/bitmap-constructor.js
+++ lab-jaren-irvine-kenneth/model/bitmap-constructor.js
@@ -3,11 +3,11 @@
 module.exports = function(data) {
   this.buffer = data; // full data for bitmap file
   this.id = data.toString('utf-8', 0, 2); // identifier for Bitmap returns BM for test comparison "doesn't mean bad manners :P"
-  this.size = data.readUInt32LE(2); // sets bitmap size
-  this.format = data.readUInt32LE(28); // sets number of bits per pixel
-  this.width = data.readUInt32LE(18); // sets file width
-  this.height = data.readUInt32LE(22); // sets file height
-  this.offset = data.readUInt32LE(10); // sets endpoint for colorArray
-  this.colorArray = data.slice(54,this.offset); // returns color array buffer
-  this.pixelArray = data.slice(this.offset, this.size); // returns pixel array buffer
+  this.size = data.readUInt32LE(2); // bitmap size
+  this.format = data.readUInt32LE(28); // number of bits per pixel
+  this.width = data.readUInt32LE(18); // image width
+  this.height = data.readUInt32LE(22); // image height
+  this.offset = data.readUInt32LE(10); // endpoint for colorArray and startpoint for pixelArray
+  this.colorArray = data.slice(54,this.offset); // color table
+  this.pixelArray = data.slice(this.offset, this.size); // pixel array
 };
